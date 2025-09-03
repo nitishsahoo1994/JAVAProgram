1d364ed8918e94fb3b1e6cd035cc77a03cff59d3
Nitish Kuamr Sahoo <nsahoo1994@yahoo.com>
2025-09-03 18:28:20 +0530

    added longest sum subarray

--- src/test/java/arrayTrain/day4/LongestSubArray.java
+++ src/test/java/arrayTrain/day4/LongestSubArray.java
@@ -1,11 +1,38 @@
 package arrayTrain.day4;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class LongestSubArray {
-    public static void main(String[] args) {
+
+    public static int longestSubarray(int[] arr,int k){
+        int n=arr.length;
+        int sum=0;
+        int maxLen=0;
         HashMap<Integer,Integer> prefixMap=new HashMap<>();
+        for (int i=0;i<n;i++){
+            sum+=arr[i];
 
+            //
+            if(sum==k){
+                maxLen=i+1;
+            }
 
+            //if (sum-k) seen before on prefixMap
+            if(prefixMap.containsKey(sum-k)){
+                int len=i-prefixMap.get(sum-k);
+                maxLen= Math.max(len,maxLen);
+            }
+
+            //keep all sum in prefixMap if not visible
+            prefixMap.putIfAbsent(sum,i);
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] array={10,5,2,7,19};
+        int length=longestSubarray(array,15);
+        System.out.printf("length is:"+length);
     }
 }
